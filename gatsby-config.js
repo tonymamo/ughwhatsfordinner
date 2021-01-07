96c8bfa6aace542409e5c3176d4826a9bc1b8e64
Tony Mamo <tony.mamo@galileo-insights.com>
2021-01-07 14:15:32 -0500

    update content in gatsby-config and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ugh, what's for dinner?`,
+    description: `A simple app to help us decide what to make or order for dinner`,
+    author: `@tonymamo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
